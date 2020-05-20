d82bf111d980ce0186e20adce24bac656410c87a
slim12345 <65578575+slim12345@users.noreply.github.com>
2020-05-20 09:50:00 +0800

    Add files via upload

---
+++ utils/api.js
@@ -0,0 +1,366 @@
+import util from "@/utils/util.js";
+
+//处理code
+function coderesult(code, msg) {
+	if (code == -1) {
+		uni.removeStorageSync('token');
+		// #ifdef APP-PLUS
+		uni.reLaunch({
+			url: '/pages/applogin/applogin'
+		})
+		// #endif
+		// #ifdef H5
+		var ua = window.navigator.userAgent.toLowerCase();
+		if (ua.match(/micromessenger/i) == 'micromessenger') {
+			util.msg('登录已超时');
+		} else {
+			uni.reLaunch({
+				url: '/pages/login/login'
+			})
+		}
+		// #endif
+		// #ifdef MP-WEIXIN
+		uni.reLaunch({
+			url: '/pages/index/index'
+		})
+		// #endif
+	} else if (code == -2) {
+		// .redirect('/pages/userbind/userbind');
+	} else {
+		util.msg(msg);
+	}
+}
+
+// 用户绑定
+function Userbind(data, success, complete) {
+	util.PostRequest('/auth/auth/bind', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//H5 APP 用户登录
+function Postlogin(data, success, complete) {
+	util.PostRequest('/auth/auth/login', data, function(res) {
+		if (res.code == 1) {
+			var data = res.data;
+			uni.setStorageSync('token', data.jwt);
+			if (success) {
+				success();
+			}
+		} else {
+			util.msg(res.msg);
+		}
+	}, complete, false);
+}
+//用户提现
+function Usertianxian(data, success, complete) {
+	util.PostRequest('/withdrawal/index/index', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+
+	}, complete, false);
+}
+//用户充值
+function Userchongzhi(data, success, complete) {
+	util.PostRequest('/index/recharge/index', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//GET获取表单数据
+function Getregister(data, success, complete) {
+	util.GetRequest('/auth/auth/reg', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//POST提交注册
+function Postregister(data, success, complete) {
+	util.PostRequest('/auth/auth/reg', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+
+//获取验证码(手机)
+function sendcodeMobile(data, success, complete) {
+	util.PostRequest('/auth/sendcode/mobile', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success();
+				util.msg(res.msg);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//获取验证码(邮箱)
+function sendcodeEmail(data, success, complete) {
+	util.PostRequest('/auth/sendcode/email ', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success();
+				util.msg(res.msg);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+
+//微信小程序用户信息登录
+function wxsmaillogin(encryptedData, iv, success, complete) {
+	// #ifdef MP-WEIXIN
+	var session_key = uni.getStorageSync('session_key');
+	util.PostRequest('/auth/auth/decryptdata', {
+		sessionKey: session_key,
+		encryptedData: encryptedData,
+		iv: iv
+	}, function(decres) {
+		if (decres.code == 1) {
+			var token = decres.data.jwt;
+			uni.setStorageSync('token', token);
+			if (success) {
+				success(decres.data);
+			}
+			console.log(decres.data.isReg);
+			
+		} else {
+			coderesult(decres.code, decres.msg);
+		}
+	}, complete, false);
+
+	// #endif
+}
+//轮播图
+function index(data, success) {
+	util.GetRequest('/banner/index/index', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, function() {
+		uni.stopPullDownRefresh();
+	}, false);
+}
+// 我的 获取用户信息
+function my(success, complete) {
+	util.GetRequest('/index/center/getuserinfo', '', function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete);
+}
+// 获取我的钱包
+function mypurse(data, success) {
+	util.GetRequest('/index/center/getmywallet', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	});
+}
+//修改手机号码
+function updatephone(data, success) {
+	util.GetRequest('/index/center/editmobile', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	});
+}
+// 上传图片
+function uploadimg(file, success, complete) {
+	util.uploadFile('/index/upload/pictures', file, 'file', {}, function(response) {
+		var res = eval("(" + response + ")");
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//上传文件
+function uploadfile(file, success, complete) {
+	util.uploadFile('/index/upload/files', file, 'file', {}, function(response) {
+		var res = eval("(" + response + ")");
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+//修改个人资料
+function updatedata(data, success, complete) {
+	util.PostRequest('/index/center/edit', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success();
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	}, complete, false);
+}
+
+//APP微信授权登录
+function wxapplogin(success) {
+	// #ifdef APP-PLUS
+	var aweixin = getApp().globalData.auths['weixin'];
+	// console.log(getApp().globalData.auths);
+	if (!aweixin) {
+		util.msg('当前环境不支持微信登录');
+		return;
+	}
+	aweixin.authorize(function(e) {
+		if (e && e.code) {
+			//console.log(e);
+			var code = e.code;
+			util.PostRequest('/auth/auth/wxapplogin', {
+				code: code,
+				pid: 0
+			}, function(res) {
+				console.log(res);
+				if (res.code == 1) {
+					var token = res.data.jwt;
+					uni.setStorageSync('token', token);
+					if (success) {
+						success(res.data);
+					}
+				} else {
+					util.msg(res.msg);
+				}
+			}, undefined, false);
+		}
+	}, function(e) {
+		util.msg('您取消了微信授权！');
+	}, {
+		scope: 'snsapi_userinfo',
+		state: '',
+		appid: getApp().globalData.appid,
+		//'wx4910300b1af06fd3'
+	});
+	// #endif
+}
+
+// 获取文章列表
+function getbooklist(data, success) {
+	util.GetRequest('/article/index/getlist', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	},function() {
+		uni.stopPullDownRefresh();
+	},false);
+}
+// 文章详情
+function getbookdetail(data, success) {
+	util.GetRequest('/article/index/getdetail', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	});
+}
+// 获取邀请列表
+function getMyInvitationList(data,success){
+	util.GetRequest('/index/Invitation/getMyInvitationList', data, function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	},function() {
+		uni.stopPullDownRefresh();
+	}, false);
+}
+//邀请好友 获取海报
+function getInviteFriends(success){
+	util.GetRequest('/index/Invitation/getInviteFriends', '', function(res) {
+		if (res.code == 1) {
+			if (success) {
+				success(res.data);
+			}
+		} else {
+			coderesult(res.code, res.msg);
+		}
+	});
+}
+export default {
+	Getregister: Getregister,
+	Postregister: Postregister,
+	sendcodeMobile: sendcodeMobile,
+	sendcodeEmail: sendcodeEmail,
+	Postlogin: Postlogin,
+	Usertianxian: Usertianxian,
+	Userbind: Userbind,
+	wxsmaillogin: wxsmaillogin,
+	Userchongzhi: Userchongzhi,
+	index: index,
+	my: my,
+	mypurse: mypurse,
+	uploadimg: uploadimg,
+	uploadfile:uploadfile,
+	updatedata: updatedata,
+	updatephone: updatephone,
+	wxapplogin: wxapplogin,
+	coderesult:coderesult,
+	getbooklist:getbooklist,
+	getbookdetail:getbookdetail,
+	getMyInvitationList:getMyInvitationList,
+	getInviteFriends:getInviteFriends
+
+}
---
+++ utils/util.js
@@ -0,0 +1,259 @@
+import md5 from "@/utils/md5.js";
+
+//弹出提示
+const msg = (title, icon = 'none', mask = false, duration = 1500) => {
+	//统一提示方便全局修改
+	if (Boolean(title) === false) {
+		return;
+	}
+	uni.showToast({
+		title,
+		duration,
+		mask,
+		icon
+	});
+	setTimeout(function() {
+		uni.hideToast();
+	}, duration);
+}
+
+//上一页面对象
+const prePage = () => {
+	let pages = getCurrentPages();
+	let prePage = pages[pages.length - 2];
+	// #ifdef H5
+	return prePage;
+	// #endif
+	return prePage.$vm;
+}
+//时间戳转日期
+//时间戳为10位需*1000，时间戳为13位的话不需乘1000
+// 一种 返回年月日时分秒
+function fomateDate(num) {
+	let date = new Date(num * 1000);
+	var Y = date.getFullYear() + '-';
+	var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+	var D = date.getDate() + ' ';
+	var h = date.getHours() + ':';
+	var m = date.getMinutes() + ':';
+	var s = date.getSeconds();
+	return Y + M + D + h + m + s;
+}
+// 二种 返回年月日
+function fomateYMD(num) {
+	let date = new Date(num * 1000);
+	var Y = date.getFullYear() + '-';
+	var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+	var D = date.getDate() + ' ';
+	if (date.getDate() < 10) {
+		D = '0' + date.getDate()
+	} else {
+		D = date.getDate()
+	}
+	return Y + M + D;
+}
+//获取当前页面对象
+function currentPage() {
+	var pages = getCurrentPages();
+	var page = pages[pages.length - 1];
+	// #ifdef H5
+	return page;
+	// #endif
+	return page.$vm;
+}
+
+//数据生成签名
+function toMd5(requestData, timestamp, nonce) {
+	// var key = getApp().globalData.api_key;
+	var key = getApp().globalData.api_key;
+	requestData['nonce'] = nonce;
+	requestData['timestamp'] = timestamp;
+	requestData['key'] = key;
+	var data = dicOrderWithData(requestData);
+	console.log(data);
+	return md5.md5(data);
+}
+//排序处理成字符串
+function dicOrderWithData(data) {
+	var result = '';
+	var keys = [];
+	for (var k in data) {
+		keys.push(k);
+	}
+	keys.sort();
+	for (var i = 0; i < keys.length; i++) {
+		var k = keys[i];
+		var v = data[k];
+		var value = '';
+		if (v == null || v == undefined) {
+			value = '';
+		} else {
+			value = v;
+		}
+		result += k + '=' + value + '&';
+	}
+	if (result.length > 1) {
+		result = result.substr(0, result.length - 1);
+	}
+	return result;
+}
+
+
+//POST方式网络请求
+function PostRequest(url, data, success, complete, isLoad = false, title = '正在加载') {
+	if (isLoad) {
+		uni.showLoading({
+			title: title,
+			mask: true
+		});
+	}
+	var requestData = {};
+	for (var k in data) {
+		requestData[k] = data[k];
+	}
+	var timestamp = Date.parse(new Date()) / 1000;
+	var nonce = Math.random().toString(36).substr(2, 15);
+	var sign = toMd5(requestData, timestamp, nonce);
+	var token = uni.getStorageSync('token');
+	var header = {
+		'content-type': 'application/x-www-form-urlencoded',
+		'jwt': token,
+		'signtoken': sign
+		// 'timestamp': timestamp,
+		// 'nonce': nonce,
+		// 'sign': sign
+	};
+
+	uni.request({
+		url: getApp().globalData.global_url + url,
+		data: data,
+		method: 'POST',
+		header: header,
+		success: function(res) {
+			//console.log(res);
+			if (res && res.statusCode == 200) {
+				success(res.data);
+			} else {
+				var json = {
+					code: -999,
+					msg: '基础网络连接错误！'
+				};
+				success(json);
+			}
+		},
+		fail: function() {
+			msg('请检查网络连接！');
+		},
+		complete: function() {
+			if (isLoad) {
+				uni.hideLoading();
+			}
+			if (complete) {
+				complete();
+			}
+		}
+	});
+}
+//GET方式网络请求
+function GetRequest(url, data, success, complete, isLoad = false, title = '正在加载') {
+	if (isLoad) {
+		uni.showLoading({
+			title: title,
+			mask: true
+		});
+	}
+	var requestData = {};
+	var queryString = '';
+	for (var k in data) {
+		requestData[k] = data[k];
+		queryString += k + '=' + data[k] + '&';
+	}
+	if (queryString.length > 1) {
+		queryString = queryString.substr(0, queryString.length - 1);
+	}
+	var timestamp = Date.parse(new Date()) / 1000;
+	var nonce = Math.random().toString(36).substr(2, 15);
+	var sign = toMd5(requestData, timestamp, nonce);
+	var token = uni.getStorageSync('token');
+	var header = {
+		'jwt': token,
+		// 'signtoken': 'sign'
+	};
+	uni.request({
+		url: getApp().globalData.global_url + url + '?' + queryString,
+		method: 'GET',
+		header: header,
+		success: function(res) {
+			//console.log(res);
+			if (res && res.statusCode == 200) {
+				success(res.data);
+			} else {
+				var json = {
+					code: -999,
+					msg: '基础网络连接错误！'
+				};
+				success(json);
+			}
+		},
+		fail: function() {
+			msg('请检查网络连接！');
+		},
+		complete: function() {
+			if (isLoad) {
+				uni.hideLoading();
+			}
+			if (complete) {
+				complete();
+			}
+		}
+	});
+}
+//上传文件
+function uploadFile(url, file, name, data, success, complete, isLoad = false, title = '正在上传') {
+	if (isLoad) {
+		uni.showLoading({
+			title: title,
+			mask: true
+		});
+	}
+	uni.uploadFile({
+		url: getApp().globalData.global_url + url,
+		filePath: file,
+		name: name,
+		formData: data,
+		// fileType:image/video/audio
+		success: function(res) {
+			// console.log(res);
+			if (res && res.statusCode == 200) {
+				success(res.data);
+			} else {
+				var json = {
+					code: -999,
+					msg: '基础网络连接错误！'
+				};
+				success(json);
+			}
+		},
+		fail: function() {
+			msg('请检查网络连接！');
+		},
+		complete: function() {
+			if (isLoad) {
+				uni.hideLoading();
+			}
+			if (complete) {
+				complete();
+			}
+		}
+	});
+}
+export default {
+	msg: msg,
+	prePage: prePage,
+	PostRequest: PostRequest,
+	GetRequest: GetRequest,
+	currentPage: currentPage,
+	uploadFile: uploadFile,
+	fomateDate: fomateDate,
+	fomateYMD: fomateYMD
+}
